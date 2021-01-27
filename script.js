445ec5e0a83ebc9354c39a2d5fb17c792f04c062
Wiktor Orda <pawior2@gmail.com>
2021-01-27 23:30:20 +0100

    Added city choosing

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-let today = new Date().toLocaleDateString();
+const today = new Date().toLocaleDateString();
 const date = document.querySelector(".date");
-let temp = document.querySelector(".temp");
+const temp = document.querySelector(".temp");
 const cisnienie = document.querySelector(".cisnienieP");
 const wiatr = document.querySelector(".wiatrP");
 const wilgotnosc = document.querySelector(".wilgotnoscP");
 const smog = document.querySelector(".smogP");
 const advice = document.querySelector(".adviceP");
 const form = document.querySelector(".btn");
-
+const cityName = document.querySelector(".city");
+const wrotenCity = document.querySelector(".form-control");
+const Url = "https://api.openweathermap.org/data/2.5/weather?q=";
+const Key = "&appid=f2799a9007994daa45c68492bae50498&units=metric";
+const apiKod = "http://kodpocztowy.intami.pl/city/";
+const city = wrotenCity;
+// End of variables
 function handleform(event) {
   event.preventDefault();
 }
@@ -20,7 +26,7 @@ smog.innerHTML = `, ${today}`;
 console.log(today);
 
 fetch(
-  "https://airapi.airly.eu/v2/measurements/installation?installationId=8824",
+  "https://airapi.airly.eu/v2/measurements/installation?installationId=8824TEST",
   {
     method: "GET",
     headers: {
@@ -39,8 +45,53 @@ fetch(
     cisnienie.innerHTML = `${Math.round(data.current.values[3].value)} hPa`;
     wilgotnosc.innerHTML = `${Math.round(data.current.values[4].value)} %`;
     smog.innerHTML = `${Math.round(data.current.values[1].value)} µg/m³`;
-    advice.innerHTML = `${data.current.indexes[0].advice} `;
+    // advice.innerHTML = `${data.current.indexes[0].advice} `; Brak w api openweather
   });
+
+function changeCity(apiUrl, apiKey, searchParam) {
+  cityName.innerHTML = `${city.value}, ${today}`;
+  fetch(apiUrl + city.value + apiKey)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      temp.innerHTML = `${Math.round(data.main.temp)} °C`;
+      wilgotnosc.innerHTML = `${Math.round(data.main.humidity)} %`;
+      cisnienie.innerHTML = `${Math.round(data.main.pressure)} hPa`;
+      const coordLon = data.coord.lon;
+      const coordLat = data.coord.lat;
+      // console.log(coordLon + " ///" + coordLat);
+      fetch(
+        `http://api.openweathermap.org/data/2.5/air_pollution?lat=${coordLat}&lon=${coordLon}&appid=f2799a9007994daa45c68492bae50498`
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          smog.innerHTML = `${Math.round(
+            data.list[0].components.pm2_5
+          )}  µg/m³`;
+        });
+    });
+  // fetch(`http://kodpocztowy.intami.pl/api/${city.value}`)
+  //   .then((response) => {
+  //     return response.json();
+  //   })
+  //   .then((data) => {
+  //     console.log(data);
+  //   });
+}
+function changePostal(Url, Key) {}
+form.addEventListener("click", function () {
+  changeCity(Url, Key, city);
+});
+//  "https://api.openweathermap.org/data/2.5/weather?q=" +
+// city.value +
+// "&appid=f2799a9007994daa45c68492bae50498&units=metric"
+//
+//
 // 52.235231, 21.017857 id: 337/259?
 // "https://airapi.airly.eu/v2/installations/nearest?lat=52.235231&lng=21.017857&maxDistanceKM=5&maxResults=3"
 //
